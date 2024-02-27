bed97e00604ad687a551ae3ea077df9deb1605c0
Petro Kharchenko <harpeter.study@zohomail.eu>
2024-02-27 13:06:37 +0200

    header alpha done

--- js/index.js
+++ js/index.js
@@ -1,10 +1,34 @@
-'use strict'
+"use strict";
 
 // import './dynamic_adapt.js';
 
-const menuIcon = document.querySelector('.menu-icon');
-const navMenu = document.querySelector('.nav-menu');
+const menuIcon = document.querySelector(".menu-icon");
+const navMenu = document.querySelector(".nav-menu");
+const header = document.querySelector(".header");
 
-menuIcon.addEventListener('click', () => {
-  navMenu.classList.toggle('menu-open');
-})
+menuIcon.addEventListener("click", () => {
+  navMenu.classList.toggle("menu-open");
+  // if (header.classList.contains('blur-header')) {
+  //   header.classList.remove('blur-header');
+  // }
+});
+
+const navLinks = document.querySelectorAll(".header-link");
+console.log(navLinks);
+
+navLinks.forEach((navLink) => {
+  navLink.addEventListener("click", () => {
+    const navMenu = document.querySelector(".nav-menu");
+
+    navMenu.classList.remove("menu-open");
+  });
+});
+
+window.addEventListener('scroll', blurHeader);
+
+function blurHeader() {
+  const header = document.querySelector(".header");
+  this.scrollY >= 50
+    ? header.classList.add("blur-header")
+    : header.classList.remove("blur-header");
+}
